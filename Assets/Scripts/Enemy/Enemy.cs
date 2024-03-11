4dd4f88c6389096c60dccea2923b652ad56e19bf
MaiTheLongDraco <128013309+MaiTheLongDraco@users.noreply.github.com>
2024-03-11 16:41:24 +0700

    - make enemy not move into slime but move nearly to slime instead

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -11,7 +11,7 @@ public class Enemy : MonoBehaviour
 	}
 	protected void ChaseSlime(Vector3 slimePos, float chaseSpeed)
     {
-        transform.position = Vector2.MoveTowards(transform.position, slimePos, chaseSpeed);
+        transform.position = Vector2.MoveTowards(transform.position, slimePos- ((slimePos-transform.position).normalized)/3, chaseSpeed);
        if(transform.position.x>=slimePos.x)
 		{
             transform.localRotation = new Quaternion(0, 180, 0, 0);
