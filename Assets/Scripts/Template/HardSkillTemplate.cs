c12a88937ebb6284e47e6b3c808d15aa89910e36
user <user>
2024-03-26 17:14:23 +0700

    - making hard skill template not be directed use anymore

--- Assets/Scripts/Template/HardSkillTemplate.cs
+++ Assets/Scripts/Template/HardSkillTemplate.cs
@@ -1,16 +1,20 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 [CreateAssetMenu(fileName = "HardSkillTemplate", menuName = "Template/HardSkillTemplate")]
 public class HardSkillTemplate : ScriptableObject
 {
     public List<HardSkillInfo> ListInfo = new();
-    public HardSkillTemplate( List<HardSkillInfo> subButtonInfos)
+
+    public HardSkillTemplate(List<HardSkillInfo> subButtonInfos)
     {
         ListInfo = subButtonInfos;
     }
+
     public HardSkillTemplate Clone()
     {
-        return new HardSkillTemplate( ListInfo);
+        var tempList = new List<HardSkillInfo>();
+        foreach (var info in ListInfo) tempList.Add(info.Clone() as HardSkillInfo);
+        return new HardSkillTemplate(tempList);
     }
 }
